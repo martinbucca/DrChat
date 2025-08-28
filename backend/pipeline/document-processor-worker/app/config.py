b17f0bc36087028e2a0b3a5c491270b02792d9c8
Martin Bucca <martinbucca02@gmail.com>
2025-08-28 13:39:45 -0300

    add config to load env var

---
+++ backend/pipeline/document-processor-worker/app/config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+VECTOR_INDEX_NAME = os.getenv('VECTOR_INDEX_NAME')
+
+NEO4J_URI = os.getenv('NEO4J_URI')
+NEO4J_USERNAME = os.getenv('NEO4J_USERNAME')
+NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD')
+
+EMBEDDINGS_MODEL = os.getenv('EMBEDDINGS_MODEL')
+
+GROQ_API_BASE = os.getenv('GROQ_API_BASE')
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+
+UNSTRUCTURED_API_KEY = os.getenv('UNSTRUCTURED_API_KEY')
+# To use ChatOpenAI from Groq, set the OPENAI_API_KEY environment variable to your Groq API key
+os.environ["OPENAI_API_KEY"] = GROQ_API_KEY
